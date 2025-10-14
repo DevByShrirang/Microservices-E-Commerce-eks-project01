1b49779986e712c1200b72dab43ae4ae4976256b
Devops_Learner <shrirang.patil1812@gmail.com>
2025-10-14 18:23:22 +0530

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -7,9 +7,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "aluruarumullaa1"
+    bucket = "mycompany-billing01-dev"
     key    = "k8/terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 
   required_version = ">= 1.6.3"
